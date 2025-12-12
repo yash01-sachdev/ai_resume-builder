08793312d2c74455e47e1d65373ce5d02bfc4b1a
yash01-sachdev <yashsachdev2003@gmail.com>
2025-12-13 02:45:50 +0530

    Initial commit - AI Resume Builder

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
+import resumeRouter from "./routes/resumeRoutes.js";
+import aiRouter from "./routes/aiRoutes.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Database connection
+await connectDB()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res)=> res.send("Server is live..."))
+app.use('/api/users', userRouter)
+app.use('/api/resumes', resumeRouter)
+app.use('/api/ai', aiRouter)
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+    
+});
